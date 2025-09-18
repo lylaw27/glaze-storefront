91b9fa152bb544aebc02b372acdd0ac53a764fc8
larrylaw <lylaw27@gmail.com>
2025-09-19 01:26:57 +0800

    first commit

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,10 +1,10 @@
 import Medusa from "@medusajs/js-sdk"
 
 // Defaults to standard port for Medusa server
-let MEDUSA_BACKEND_URL = process.env.MEDUSA_BACKEND_URL || "http://localhost:9000"
+let MEDUSA_BACKEND_URL = "http://localhost:9000"
 
-if (process.env.MEDUSA_BACKEND_URL) {
-  MEDUSA_BACKEND_URL = process.env.MEDUSA_BACKEND_URL
+if (process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL) {
+  MEDUSA_BACKEND_URL = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL
 }
 
 export const sdk = new Medusa({
